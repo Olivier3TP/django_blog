c3f7fe6a6f672cc639cf95b143926e030c77fd23
Przemek B <pprzemek89@gmail.com>
2024-02-29 14:59:37 +0100

    kolejny commit

--- aplikacjablog/views.py
+++ aplikacjablog/views.py
@@ -1,10 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from aplikacjablog.models import Post
 
 
 # Create your views here.
+
 def post_list(request):
     posts = Post.objects.all()
-    return 'test 123'
-   # return render(request,'')
+    return render(request, 'blog/post/list.html')
