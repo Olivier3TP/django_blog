b998fd4208423296087b6b59cbc1f6aa53f77f0d
Przemek B <pprzemek89@gmail.com>
2024-03-14 14:32:43 +0100

    kolejny commit

--- aplikacjablog/views.py
+++ aplikacjablog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from aplikacjablog.models import Post
 
@@ -7,5 +7,11 @@ from aplikacjablog.models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post/list.html')
+    posts = Post.objects.all().filter(status='published')
+    return render(request, 'blog/post/list.html',
+                  {'posts': posts})
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404()
+
+
+
