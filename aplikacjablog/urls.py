3a75991f5106b4cd2aeecda227f4fd7f19f24b90
Przemek B <pprzemek89@gmail.com>
2024-03-14 15:04:17 +0100

    kolejny commit

--- aplikacjablog/urls.py
+++ aplikacjablog/urls.py
@@ -5,5 +5,6 @@ from aplikacjablog import views
 app_name = 'aplikacjablog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>', views.post_detail, name='post_detail')
 ]
--- aplikacjablog/views.py
+++ aplikacjablog/views.py
@@ -11,7 +11,6 @@ def post_list(request):
     return render(request, 'blog/post/list.html',
                   {'posts': posts})
 def post_detail(request, year, month, day, slug):
-    post = get_object_or_404()
-
-
-
+    post = get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html',
+                  {'post': post})
