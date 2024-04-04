508a5d453082cbbe10cc7b2bcb87710191ad39b4
Przemek B <pprzemek89@gmail.com>
2024-04-04 14:29:36 +0200

    kolejny commit

--- aplikacjablog/urls.py
+++ aplikacjablog/urls.py
@@ -5,6 +5,7 @@ from aplikacjablog import views
 app_name = 'aplikacjablog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post-list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>', views.post_detail, name='post_detail')
 ]
--- aplikacjablog/views.py
+++ aplikacjablog/views.py
@@ -1,3 +1,5 @@
+from msilib.schema import ListView
+
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -6,10 +8,17 @@ from aplikacjablog.models import Post
 
 # Create your views here.
 
-def post_list(request):
-    posts = Post.objects.all().filter(status='published')
-    return render(request, 'blog/post/list.html',
-                  {'posts': posts})
+# def post_list(request):
+#     posts = Post.objects.all().filter(status='published')
+#     return render(request, 'blog/post/list.html',
+#                   {'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.objects.all().filter(status='published')
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html',
